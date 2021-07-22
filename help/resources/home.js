aa665fce33e49d4b0853a0e1b1b2bff81af904e8
bestrayni <42503136+bestrayni@users.noreply.github.com>
2021-07-22 21:23:15 +0200

    s

---
+++ help/resources/home.js
@@ -0,0 +1,4 @@
+//var kleks = "gowno"
+//var ilosc = 12
+//var uczucie = "miekkosci"
+//alert("Dzisiaj znow zrobiłem " + kleks + " , wyszło tego az " + ilosc + " kg jednak teraz towarzyszy mi uczucie " + uczucie)
